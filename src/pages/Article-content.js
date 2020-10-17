03d970cd2fdb98e75ed8e7ad2ee3a1ab0bcbb6cb
bsrinivas064 <bsrinivas064@gmail.com>
2020-10-17 16:02:22 +0000

    Added 5 files to /src/pages

---
+++ src/pages/Article-content.js
@@ -0,0 +1,21 @@
+const articles = [{
+    name: 'learn-react',
+    title: 'Fast way to learn react',
+    content: [
+        `There is a trend in software development that I am starting to despise from the deepest 
+        corners of my liver — portraying complicated stuff as if it were a piece o’ cake. React 
+        is probably one of the most famous ones out there in terms of JavaScript frameworks and 
+        libraries. From the absolute ludicrous “Learn React in 5 Minutes” (note, that’s for a 6 
+        minute read article) published by no other than the highly acclaimed FreeCodeCamp.`,
+
+        `“Learn React in 7 Minutes” published on Egghead, “Learn React in 8 Minutes” (coincidentally 
+        written by the same author as the one who boasts you can do it in 5 minutes; so is it 5 
+        or 8 minutes now?!?), “Learn the Basics of React in 10 minutes” posted on LinkedIn by 
+        some other overly optimistic dude, to less aggressive ones like “Learn React in 1 Hour” 
+        published on Udemy and Learn React in an Afternoon or Two — this last one nearly getting it right. 
+        To top it all off, statements such as the one below are thrown around willy-nilly:`
+    ]
+},
+];
+
+export default articles;
---
+++ src/pages/ArticleList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ArticleList = () => (
+    <React.Fragment>
+        
+    <h1>Articles</h1>
+    
+    </React.Fragment>
+);
+
+export default ArticleList;
